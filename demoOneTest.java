c6a37a93dadac37a1800ce38b52cf5db9cd6ad9d
asaini27 <asaini@ucsd.edu>
2022-05-01 22:44:49 -0700

    Update demoOneTest.java

--- demoOneTest.java
+++ demoOneTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class demoOneTest {
     @Test
     public void addition() {
-        assertEquals(2, demoOne.add(1 , 3));
+        assertEquals(2, demoOne.add(1 , 1));
     }
 }
